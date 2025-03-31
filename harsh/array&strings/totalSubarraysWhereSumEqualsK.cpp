f9a0d62abae6011f4452932d6e223be17b3628dc
Harsh72019 <harshbali374@gmail.com>
2025-03-31 15:34:41 +0530

    count total subarrays where sum euqals k

---
+++ harsh/array&strings/totalSubarraysWhereSumEqualsK.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int subarraySum(vector<int>& nums, int k) {
+    unordered_map<int , int> um;
+    um[0] = 1;
+    int ans = 0;
+    int prefixSum = 0;
+    for(int i = 0 ; i < nums.size() ; i++)
+    {
+        prefixSum += nums[i];
+        int compliment = prefixSum - k;
+        if(um.find(compliment) != um.end())
+        {
+            ans += um[compliment];
+        }
+        um[prefixSum]++; 
+        
+    }
+    return ans;
+}
