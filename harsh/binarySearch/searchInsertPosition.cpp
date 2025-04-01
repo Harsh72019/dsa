a6bde512f1cb9845ee2c93810a645a162912b54a
Harsh72019 <harshbali374@gmail.com>
2025-04-01 20:28:49 +0530

    search insert position

--- harsh/binarySearch/searchInsertPosition.cpp
+++ harsh/binarySearch/searchInsertPosition.cpp
@@ -3,24 +3,21 @@ using namespace std;
 
 
 
-// Almost similar implemenetation to lowerbound
-
-int searchPosition(vector<int> arr , int target){
-    int n = arr.size();
-    int low = 0 ; 
-    int high = arr.size()-1;
-    int ans = arr.size();
+int searchInsert(vector<int>& nums, int target) {
+    int n = nums.size();
+    int low = 0;
+    int high = n - 1;
     while(low <= high)
     {
-        int mid = low + (high - low) /2;
-        if(arr[mid] >= target)
+        int mid = low + (high - low)/2;
+        if(nums[mid] >= target)
         {
-            high  = mid - 1;
-            ans = mid;
-        }
-        else{
+            high = mid -1;
+        }   
+        else
+        {
             low = mid + 1;
         }
     }
-    return ans;
+    return low;
 }
