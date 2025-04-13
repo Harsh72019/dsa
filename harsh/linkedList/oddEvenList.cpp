b46e7100a6d3faab027d774002598d1c4d2c3ac9
harsh <harshbali374@gmail.com>
2025-04-13 16:05:57 +0530

    odd even list

---
+++ harsh/linkedList/oddEvenList.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* oddEvenList(ListNode* head) {
+    if(!head || !head->next)
+        return head;
+    ListNode* odd = head;
+    ListNode* even = head->next;
+    ListNode* evenHead = even;
+    ListNode* newHead = odd;
+
+    while(even && even->next)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        even = even->next;
+    }
+    odd->next = evenHead;
+    return newHead;
+}
