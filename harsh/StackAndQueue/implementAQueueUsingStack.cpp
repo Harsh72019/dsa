928b5f8a26ff6a6c59ae45094bdad5bffc978386
harsh <harshbali374@gmail.com>
2025-04-30 23:15:53 +0530

    implement a queue using stack

--- harsh/StackAndQueue/implementAQueueUsingStack.cpp
+++ harsh/StackAndQueue/implementAQueueUsingStack.cpp
@@ -1,41 +1,37 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class MyQueue {
-    Stack<Integer> first;
-    Stack<Integer> second;
-    public MyQueue() {
-        first = new Stack<>();
-        second = new Stack<>();
-    }
-    
-    public void push(int x) {
-        while(!first.isEmpty())
-        {
-            second.push(first.peek());
-            first.pop();
-
+    public:
+        stack<int> s1;
+        stack<int> s2;
+        MyQueue() {
         }
-        first.push(x);
-        while(!second.isEmpty())
-        {
-            first.push(second.peek());
-            second.pop();
+        
+        void push(int x) {
+            while(!s2.empty()){
+                s1.push(s2.top());
+                s2.pop();
+            }
+    
+            s1.push(x);
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
         }
-    }
+        
+        int pop() {
+            int temp = s2.top();
+            s2.pop();
+            return temp;
+        }
+        
+        int peek() {
+            return s2.top();
+        }
+        
+        bool empty() {
+            if(s2.empty())
+                return true;
+            return false;
+        }
+    };
     
-    public int pop() {
-        int popped = first.peek();
-        first.pop();
-        return popped;
-    }
-    
-    public int peek() {
-        return first.peek();
-    }
-    
-    public boolean empty() {
-        return first.isEmpty();
-    }
-}
-
