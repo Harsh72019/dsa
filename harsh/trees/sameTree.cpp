d27e55a0435f8f4270230218f526c3b6314d76f7
harsh <harshbali374@gmail.com>
2025-06-07 15:44:29 +0530

    is same tree

--- harsh/trees/sameTree.cpp
+++ harsh/trees/sameTree.cpp
@@ -15,5 +15,12 @@ bool isSameTree(TreeNode* p, TreeNode* q)
     if (!p && !q) return true;
     if (!p || !q) return false;
     if (p->val != q->val) return false;
-    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right) && p->val == q->val;
+}
+
+int main(){
+    TreeNode *root1 = new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4), new TreeNode(5)));// 1 2 3 4 5
+    TreeNode *root2 = new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4), new TreeNode(5)));// 1 2 3 4 6
+    cout << isSameTree(root1, root2);
+    return 0;
 }
