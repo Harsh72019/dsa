78a71d58bf2254f8456984ce35714eba133a05cf
harsh <harshbali374@gmail.com>
2025-06-10 00:21:23 +0530

    lowest common ancestor

---
+++ harsh/trees/LCAinBinaryTree.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root==NULL) return NULL;
+    if(root==p || root==q) return root;
+    TreeNode* left = lowestCommonAncestor(root->left,p,q);
+    TreeNode* right = lowestCommonAncestor(root->right,p,q);
+    if(left==NULL) return right;
+    if(right==NULL) return left;
+    return root;
+}
