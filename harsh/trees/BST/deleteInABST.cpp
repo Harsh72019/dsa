61ccb44314adbd69c773fd75fab60e9c49d20517
harsh <harshbali374@gmail.com>
2025-06-14 15:44:31 +0530

    delete in a binary search tree

---
+++ harsh/trees/BST/deleteInABST.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+ TreeNode* findMin(TreeNode* node) {
+        while (node->left)
+            node = node->left;
+        return node;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return nullptr;
+
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            } else if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            } else {
+                TreeNode* minNode = findMin(root->right);
+                root->val = minNode->val;
+                root->right = deleteNode(root->right, minNode->val);
+            }
+        }
+
+        return root;
+    }
