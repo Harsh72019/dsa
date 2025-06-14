41e4c05ba176033e8589a496936401420c9b73e1
harsh <harshbali374@gmail.com>
2025-06-14 15:59:40 +0530

    1

--- harsh/trees/BST/deleteInABST.cpp
+++ harsh/trees/BST/deleteInABST.cpp
@@ -1,40 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct TreeNode {
+struct TreeNode
+{
     int val;
     TreeNode *left;
     TreeNode *right;
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
- TreeNode* findMin(TreeNode* node) {
-        while (node->left)
-            node = node->left;
-        return node;
+TreeNode *findMin(TreeNode *node)
+{
+    while (node->left)
+        node = node->left;
+    return node;
+}
+
+TreeNode *deleteNode(TreeNode *root, int key)
+{
+    if(!root)
+        return NULL;
+    if(root->val > key)
+    {
+        root->left = deleteNode(root->left , key);
     }
-
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if (!root) return nullptr;
-
-        if (key < root->val) {
-            root->left = deleteNode(root->left, key);
-        } else if (key > root->val) {
-            root->right = deleteNode(root->right, key);
-        } else {
-            if (!root->left) {
-                TreeNode* temp = root->right;
-                delete root;
-                return temp;
-            } else if (!root->right) {
-                TreeNode* temp = root->left;
-                delete root;
-                return temp;
-            } else {
-                TreeNode* minNode = findMin(root->right);
-                root->val = minNode->val;
-                root->right = deleteNode(root->right, minNode->val);
-            }
+    else if(root->val < key)
+    {
+        root->right = deleteNode(root->right , key);
+    }
+    else{
+        if(!root->left)
+        {
+            TreeNode* temp = root->right;
+            delete root;
+            return temp;
         }
-
-        return root;
+        else if(!root->right)
+        {
+            TreeNode* temp = root->left;
+            delete root;
+            return temp;
+        }
+        else{
+            TreeNode * temp = findMin(root->right);
+            root->val = temp->val;
+            root->right = deleteNode(root->right , temp->val);
+        }
     }
+}
