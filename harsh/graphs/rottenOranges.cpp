d0806e8e8991ebbe0f291e1a57cb86b8bb7f333c
harsh <harshbali374@gmail.com>
2025-06-18 00:53:37 +0530

    rotten oranges

--- harsh/graphs/rottenOranges.cpp
+++ harsh/graphs/rottenOranges.cpp
@@ -2,41 +2,60 @@
 using namespace std;
 
  int orangesRotting(vector<vector<int>>& grid) {
-        int rows = grid.size(), cols = grid[0].size();
-        queue<pair<int, int>> q;
-        int fresh = 0;
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 2)
-                    q.push({i, j});
-                else if (grid[i][j] == 1)
-                    fresh++;
+        int row = grid.size();
+        int col = grid[0].size();
+        queue<pair<int , int>> q;
+        vector<vector<bool>> visited(row , vector<bool>(col , false));
+        int freshCount = 0;
+
+        for(int i = 0 ; i < row ; i++)
+        {
+            for(int j = 0 ; j < col ; j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    q.push({i , j});
+                    visited[i][j] = true;
+                }
+                else if(grid[i][j] == 1)
+                {
+                    freshCount++;
+                }
             }
         }
 
-        if (fresh == 0) return 0; 
-
-        int minutes = -1;
+        if(freshCount == 0) return 0;
+        int time = 0;
         vector<int> dx = {0, 0, 1, -1};
         vector<int> dy = {1, -1, 0, 0};
 
-        while (!q.empty()) {
+        while(!q.empty())
+        {
             int size = q.size();
-            minutes++;
+            time++;
 
-            for (int i = 0; i < size; i++) {
-                auto [x, y] = q.front(); q.pop();
-                for (int d = 0; d < 4; d++) {
-                    int nx = x + dx[d], ny = y + dy[d];
-                    if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && grid[nx][ny] == 1) {
-                        grid[nx][ny] = 2;
-                        fresh--;
-                        q.push({nx, ny});
+            for(int i = 0 ; i < q.size() ; i++)
+            {
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+
+                for(int j = 0 ; j < 4 ; j++)
+                {
+                    int newX = x + dx[j];
+                    int newY = y + dy[j];
+
+                    if(newX >= 0 && newX < row && newY >= 0 && newY < col && 
+                       grid[newX][newY] == 1 && !visited[newX][newY])
+                    {
+                        visited[newX][newY] = true;
+                        grid[newX][newY] = 2;
+                        freshCount--;
+                        q.push({newX, newY});
                     }
                 }
             }
         }
 
-        return fresh == 0 ? minutes : -1;
+        return freshCount == 0 ? time - 1 : -1;
     }
