4debc369d1764e3edef925e16fad6baa12f2e9b3
harsh <harshbali374@gmail.com>
2025-06-21 13:42:14 +0530

    word ladder 1

---
+++ harsh/graphs/wordLadder1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin() , wordList.end());
+        queue<pair<string , int>> q;
+        q.push({beginWord , 1});
+        st.erase(beginWord);
+        while(!q.empty())
+        {
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            if(word == endWord)
+                return steps;
+            for(int i = 0 ; i < word.size() ; i++)
+            {
+                char og = word[i];
+                for(char j = 'a' ; j <= 'z' ; j++)
+                {
+                    word[i] = j;
+                    if(st.find(word) != st.end())
+                    {
+                        st.erase(word);
+                        q.push({word , steps + 1});
+                    }
+                    word[i] = og;
+                }
+            }
+        }
+        return 0;
+    }
