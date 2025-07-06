cbc168047ff476c21a551e88762370989132aa86
harsh <harshbali374@gmail.com>
2025-07-06 17:56:21 +0530

    frog jump

---
+++ harsh/dynamicProgramming/frogJump.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solver(vector<int> & heights , int index,  vector<int>& dp)
+{
+    if(index == 0)  return 0;
+    if(dp[index] != -1)    return dp[index];
+    int oneStep = solver(heights , index - 1 , dp) + abs(heights[index] - heights[index - 1]);
+    int twoStep = INT_MAX;
+
+    if(index > 1) twoStep = solver(heights , index - 2 , dp) + abs(heights[index] - heights[index - 2]);
+
+    return dp[index] =  min(oneStep , twoStep);
+
+}
+
+int frogJump(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> dp(n , -1);
+        return solver(heights , n -1 , dp);
+}
