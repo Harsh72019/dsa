62c1825f23acca173d76a466f3744ea2f208b5bc
harsh <harshbali374@gmail.com>
2025-07-29 23:54:36 +0530

    number of longest inceasing subsequence

---
+++ harsh/dynamicProgramming/numberOfLongestIncreasingSubsequence.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> size (n, 1), freq(n, 1);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(nums[i] > nums[j] && size[i] == 1 + size[j]) freq[i] += freq[j];
+                else if(nums[i]>nums[j] && 1+size[j]>size[i]){
+                    size[i] = 1 + size[j];
+                    freq[i] = freq[j];
+                }
+            }
+        }
+        int maxSize = *max_element(size.begin(), size.end());
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            if(size[i] == maxSize) ans += freq[i];
+        }
+        return ans;
+    }
