0f6bd86affc8047d1423f1199334a2dc92d00e6b
harsh <harshbali374@gmail.com>
2025-08-02 00:06:06 +0530

    mcm optimized

--- harsh/dynamicProgramming/matrixChainMultiplication.cpp
+++ harsh/dynamicProgramming/matrixChainMultiplication.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 
-int solver(int i , int j , vector<int>& arr)
+int solver(int i , int j , vector<int>& arr ,vector<vector<int>> & dp)
 {
     if(i == j )
         return 0;
+    if(dp[i][j] != -1)
+        return dp[i][j];
     int ans = INT_MAX;
     for(int k = 0 ; k <= j ; k++)
     {
-        int steps = solver(i , k , arr) + solver(k + 1 , j , arr) + arr[i - 1] * arr[k] * arr[j];
+        int steps = solver(i , k , arr , dp) + solver(k + 1 , j , arr  , dp) + arr[i - 1] * arr[k] * arr[j];
         ans = min(ans , steps);
     }
-    return ans;
+    return  dp[i][j] = ans;
 }
 
 int mcm(vector<int>& arr)
 {
     int n = arr.size() - 1;
-    return solver(1 , n , arr);
+    vector<vector<int>> dp(n , vector<int>(n , -1));
+    return solver(1 , n , arr , dp);
 }
