97b6837f3203c0acc9345c71196e8d7a3f32d952
Harsh Bali <harshbali374@gmail.com>
2025-09-28 16:31:18 +0530

    string encoding

---
+++ harsh/pbcSprint/stringEncoding.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+string encode(string &message)
+{
+    string ans =  "";
+    char curr = message[0];
+    int counter = 1;
+    for(int i = 1 ; i < message.size() ; i++)
+    {
+        if(message[i] == curr)
+        {
+            counter++;
+        }
+        else{
+            ans += curr;
+            ans += to_string(counter);
+            curr = message[i];
+            counter = 1;
+        }
+    }
+    ans += curr;
+    ans += to_string(counter);
+    return ans;
+}
