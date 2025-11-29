7c77989fe663813f4898ec028cd7cd0c7c39e3f0
Harsh Bali <harshbali374@gmail.com>
2025-11-29 23:34:56 +0530

    LC 875 Koko Eating Bananas

---
+++ brutalSprint/WEEK2/LC875.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool check(vector<int> &piles, int h, int k)
+{
+    long long total = 0;
+    for (int p : piles)
+    {
+        total += (p + k - 1) / k;
+        if (total > h)
+            return false;
+    }
+    return true;
+}
+
+int minEatingSpeed(vector<int> &piles, int h)
+{
+    int low = 1;
+    int high = *max_element(piles.begin(), piles.end());
+    int ans = high;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (check(piles, h, mid))
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
