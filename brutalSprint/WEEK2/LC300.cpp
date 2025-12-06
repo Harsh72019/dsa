2361a17615cb9dbf10ab7cd5d3dc732a0ee18ba2
Harsh Bali <harshbali374@gmail.com>
2025-12-06 17:57:53 +0530

    LC 300 Longests increasing subesquence

---
+++ brutalSprint/WEEK2/LC300.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solver(const vector<int> &nums, int index, int prevIndex, vector<vector<int>> &dp)
+{
+    if (index == nums.size())
+        return 0;
+    int &mem = dp[index][prevIndex + 1];
+    if (mem != -1)
+        return mem;
+
+    int notTake = solver(nums, index + 1, prevIndex, dp);
+
+    int take = 0;
+    if (prevIndex == -1 || nums[index] > nums[prevIndex])
+        take = 1 + solver(nums, index + 1, index, dp);
+
+    mem = max(take, notTake);
+    return mem;
+}
+
+int lengthOfLIS(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+    return solver(nums, 0, -1, dp);
+}
