b661bcf9098b23922a494c98c068900a0513d677
Harsh Bali <harshbali374@gmail.com>
2026-01-05 10:02:43 +0530

    LC 46 Permutations

--- brutalSprint/WEEK4/LC46.cpp
+++ brutalSprint/WEEK4/LC46.cpp
@@ -21,3 +21,40 @@ vector<vector<int>> permute(vector<int> &nums)
     solve(0, nums, ans);
     return ans;
 }
+
+
+
+// Done by me
+
+class Solution {
+public:
+    void solve(vector<int>& nums, vector<bool>& used,
+               vector<int>& path, vector<vector<int>>& ans) {
+
+        if (path.size() == nums.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            path.push_back(nums[i]);
+
+            solve(nums, used, path, ans);
+
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<bool> used(nums.size(), false);
+        vector<int> path;
+
+        solve(nums, used, path, ans);
+        return ans;
+    }
+};
