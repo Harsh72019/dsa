e0e366a554d671c43152cd83583ac6eb4bf923ea
Harsh Bali <harshbali374@gmail.com>
2026-02-01 20:07:22 +0530

    LC 907 Sum of Subarray Minimums

---
+++ finalSprint/LC907.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumSubarrayMins(vector<int> &arr)
+{
+    const int MOD = 1e9 + 7;
+    int n = arr.size();
+
+    vector<int> prev(n), next(n);
+    stack<int> st;
+
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && arr[st.top()] > arr[i])
+            st.pop();
+        prev[i] = st.empty() ? -1 : st.top();
+        st.push(i);
+    }
+
+    while (!st.empty())
+        st.pop();
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty() && arr[st.top()] >= arr[i])
+            st.pop();
+        next[i] = st.empty() ? n : st.top();
+        st.push(i);
+    }
+
+    long long ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        long long left = i - prev[i];
+        long long right = next[i] - i;
+        ans = (ans + arr[i] * left % MOD * right % MOD) % MOD;
+    }
+
+    return ans;
+}
