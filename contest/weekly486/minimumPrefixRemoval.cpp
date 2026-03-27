e610a044ef0279ca845b51854b067d950de622ac
Harsh72019 <harshbali374@gmail.com>
2026-03-27 18:44:12 +0530

    Minimum Prefix Removal to Make Array Strictly Increasing

---
+++ contest/weekly486/minimumPrefixRemoval.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minimumPrefixLength(vector<int> &nums)
+{
+    int n = nums.size();
+    for (int i = n - 1; i > 0; i--)
+    {
+        if (nums[i] <= nums[i - 1])
+        {
+            return i;
+        }
+    }
+    return 0;
+}
