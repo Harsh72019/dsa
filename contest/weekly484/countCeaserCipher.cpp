5c438b5d56950e48cc07f2d69be081bfd871040a
Harsh72019 <harshbali374@gmail.com>
2026-04-01 19:01:21 +0530

    Count Ceaser Cipher

---
+++ contest/weekly484/countCeaserCipher.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    long long countPairs(vector<string> &words)
+    {
+        unordered_map<string, int> map;
+        long long count = 0;
+
+        for (const string &word : words)
+        {
+            string normalized(word.length(), 'a');
+            int shift = word[0] - 'a';
+
+            for (int i = 0; i < word.length(); i++)
+            {
+                normalized[i] = 'a' + (word[i] - 'a' - shift + 26) % 26;
+            }
+
+            count += map[normalized];
+            map[normalized]++;
+        }
+
+        return count;
+    }
+};
